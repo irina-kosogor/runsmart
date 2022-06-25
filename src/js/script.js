400eb6476973065d9a42190f3b3a6b710e35fb5a
Iryna Kosogor <i.v.kosogor@gmail.com>
2022-06-25 11:41:48 +0300

    Form validation is added

--- src/js/script.js
+++ src/js/script.js
@@ -53,25 +53,54 @@ $(document).ready(function(){
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
 
+    // Modals
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i) {
+      $(this).on('click', function() {
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      });
+    });
+
+
+    function validateForms(form) {
+      $(form).validate({
+        rules: {
+          name: {
+            required: true,
+            minlength: 3
+          },
+          phone: "required",
+          email: {
+            required: true,
+            email: true
+          }
+        },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите минимум {0} символа")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Ваш адрес должен быть в формате name@domain.com"
+  
+          }
+        }
+      });
+    }
+
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+    validateForms('#order form');
+  
 });
-
-
-
-// Tiny Slider
-
-// const slider = tns({
-//     container: '.carousel__inner',
-//     items: 1,
-//     slideBy: 'page',
-//     autoplay: false,
-//     controls: false,
-//     nav: false,
-// });
-
-// document.querySelector('.prev').addEventListener('click', function () {
-//     slider.goTo('prev');
-//   });
-
-// document.querySelector('.next').addEventListener('click', function () {
-//     slider.goTo('next');
-//   }); 
