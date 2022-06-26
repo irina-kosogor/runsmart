75525b7b1d13bc6c36742841046fd09e85235106
Iryna Kosogor <i.v.kosogor@gmail.com>
2022-06-26 16:39:02 +0300

    arrow up is added

--- src/js/script.js
+++ src/js/script.js
@@ -105,4 +105,24 @@ $(document).ready(function(){
 
   $('input[name=phone]').mask("+38 (999) 999-99-99");
   
+  $('form').submit(function (e){
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+
+      $('form').trigger('reset');
+      return false;
+    });
+  });
 });
